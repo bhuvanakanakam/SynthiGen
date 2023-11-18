868857f009e607461226e477e6feed9dfd60d981
Bhuvana Teja Kanakam <116140522+bhuvanakanakam@users.noreply.github.com>
2023-11-18 21:40:28 +0530

    Delete src directory

--- src/containers/index.js
+++
@@ -1,4 +0,0 @@
-
-export { default as Footer } from './footer/Footer';
-export { default as Header } from './header/Header';
-export { default as WhatSynthiGen } from './whatSynthiGen/WhatSynthiGen';
